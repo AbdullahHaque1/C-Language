1e5179510adf302d8e32b95914ac9f584f6b00ef
AbdullahHaque1 <100842119+AbdullahHaque1@users.noreply.github.com>
2023-01-06 19:53:07 +0530

    Checking number is palindrom or not

---
+++ palindrom_or_not.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, ans = 0, b;
+    scanf("%d", &n);
+    b = n;
+    while (n != 0)
+    {
+        int d = n % 10;
+        ans = ans * 10 + d;
+        n = n / 10;
+    }
+    if (ans == b)
+    {
+        printf("Entered Number is Palindrom");
+    }
+    else
+    {
+        printf("Entered Number is Not Palindrom");
+    }
+
+    return 0;
+}
